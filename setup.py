0d2dfff2417e8540546491c9fb4a9a2040b6bbda
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-01-08 14:15:13 +0100

    version 0.7.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 from setuptools import setup, Extension
 import setuptools.command.build_py
 
-UTM_VERSION = '0.7.3'
+UTM_VERSION = '0.7.4'
 PACKAGE_NAME = 'tmGrammar'
 PACKAGE_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), PACKAGE_NAME))
 
